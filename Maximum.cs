3630125ee3c7912faf0a904962935df92d446634
Subhash Barela <barelasubhash16@gmail.com>
2023-02-22 23:43:32 +0530

    Refactore three type using generic methode

--- Maximum.cs
+++ Maximum.cs
@@ -6,32 +6,87 @@ using System.Threading.Tasks;
 
 namespace GenericMethode
 {
-    internal class Maximum
-    {
+    public class Maximum<T> where T : IComparable
+    { 
+
+        public T firstValue, secondValue, thirdValue;
+    
+        public Maximum(T firstValue, T secondValue, T thirdValue) {
+        this.firstValue = firstValue;
+        this.secondValue = secondValue;
+       this.thirdValue = thirdValue;
+    }
         
-        public static String MaxIntNumber(String firstValue, String seconValue, String thirdValue)
+        public   T MaxOfThree()
         {
            
-            if(firstValue.CompareTo(seconValue)>0 && firstValue.CompareTo(thirdValue) >0 ||
-                firstValue.CompareTo(seconValue) >= 0 && firstValue.CompareTo(thirdValue) > 0 ||
-                firstValue.CompareTo(seconValue) > 0 && firstValue.CompareTo(thirdValue) >= 0)
+            if(firstValue.CompareTo(secondValue) >0 && firstValue.CompareTo(thirdValue) >0)
+               
             {
                 return firstValue;
             }
-            if (seconValue.CompareTo(firstValue) > 0 && seconValue.CompareTo(thirdValue) > 0 ||
-                seconValue.CompareTo(firstValue) >= 0 && seconValue.CompareTo(thirdValue) > 0 ||
-                seconValue.CompareTo(firstValue) > 0 && seconValue.CompareTo(thirdValue) >= 0)
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0 )
+               
             {
-                return seconValue;
+             return secondValue;
+        }
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0 )
+               
+            {
+            return thirdValue;
+        }
+            return default;
+
+          
+        }
+       
+
+//********************************************************************************************************
+      /*  public static double MaxOfThree(double firstValue, double secondValue, double thirdValue)
+        {
+
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+
+            {
+                return firstValue;
             }
-            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(seconValue) > 0 ||
-                thirdValue.CompareTo(firstValue) >= 0 && thirdValue.CompareTo(seconValue) > 0 ||
-                thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(seconValue) >= 0)
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+
+            {
+                return secondValue;
+            }
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+
             {
                 return thirdValue;
             }
+            return 0;
 
-            return null;
+
         }
+        //**************************************************************************************************************
+
+        public static String MaxOfThree(String firstValue, String secondValue, String thirdValue)
+        {
+
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+
+            {
+                return firstValue;
+            }
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+
+            {
+                return secondValue;
+            }
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+
+            {
+                return thirdValue;
+            }
+            return null;
+
+
+        }*/
     }
 }
--- Program.cs
+++ Program.cs
@@ -13,19 +13,29 @@ namespace GenericMethode
 
         static void Main(string[] args)
         {
-            Console.WriteLine("insert the First String :");
-           String value1=(Console.ReadLine());
-            Console.WriteLine("insert the Second String :");
-            String value2 = (Console.ReadLine());
-            Console.WriteLine("insert the third String :");
-            String value3 = (Console.ReadLine());
+            Maximum<int> Obj1 = new Maximum<int>(3, 5, 6);
+            int max1 = Obj1.MaxOfThree();
+            Console.WriteLine("{0} String is gretest of other String :", max1);
+            Console.WriteLine("\n**********************************\n");
 
+           
+            
+            Maximum<double> DoubleValue = new Maximum<double>(3.4, 5.5, 6.78);
+            double max2 = DoubleValue.MaxOfThree();
+            Console.WriteLine("{0} String is gretest of other String :", max2);
+            Console.WriteLine("\n**********************************\n");
 
-            String result = Maximum.MaxIntNumber(value1, value2, value3);
-            Console.WriteLine("{0} String is gretest of other String :",result);
+            
+            
+            Maximum<String> StringValue = new Maximum<String>("234", "543", "654");
+            String max3= StringValue.MaxOfThree();
+            Console.WriteLine("{0} String is gretest of other String :",max3);
+           
+
 
 
             Console.ReadLine();
+          
         }
     }
 }
